28eec6aa5a656ae4ac194ba0afd06f37b8711faa
flashmike <mike@flashmike.com>
2020-04-19 15:51:40 +0400

    Spawning Sushi with gravity

---
+++ SorrySushi/Assets/Scripts/LineCreator.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour
+{
+    int vertextCount = 0;
+    bool mouseDown = false;
+    LineRenderer line;
+
+    void Awake()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //detect running platform if android... :mike
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                {
+#pragma warning disable CS0618 // Type or member is obsolete
+                    line.SetVertexCount(vertextCount + 1);
+#pragma warning restore CS0618 // Type or member is obsolete
+                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    line.SetPosition(vertextCount, mousePos);
+                    vertextCount++;
+
+                    BoxCollider2D box = gameObject.AddComponent<BoxCollider2D>();
+                    box.transform.position = line.transform.position;
+                    box.size = new Vector2(0.1f, 0.1f);
+                }
+
+                if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    vertextCount = 0;
+#pragma warning disable CS0618 // Type or member is obsolete
+                    line.SetVertexCount(0);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+                    BoxCollider2D[] colliders = GetComponents<BoxCollider2D>();
+
+                    foreach (BoxCollider2D box in colliders)
+                        Destroy(box);
+                }
+            }
+        }
+        else if (Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+
+
+            //mike: ...otherwise run this PC version control
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                mouseDown = true;
+            }
+
+            if (mouseDown)
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                line.SetVertexCount(vertextCount + 1);
+#pragma warning restore CS0618 // Type or member is obsolete
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                line.SetPosition(vertextCount, mousePos);
+                vertextCount++;
+
+                BoxCollider2D box = gameObject.AddComponent<BoxCollider2D>();
+                box.transform.position = line.transform.position;
+                box.size = new Vector2(0.1f, 0.1f);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                mouseDown = false;
+                vertextCount = 0;
+#pragma warning disable CS0618 // Type or member is obsolete
+                line.SetVertexCount(0);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+                BoxCollider2D[] colliders = GetComponents<BoxCollider2D>();
+
+                foreach (BoxCollider2D box in colliders)
+                    Destroy(box);
+            }
+        }
+    }
+}
---
+++ SorrySushi/Assets/Scripts/SushiSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SushiSpawner : MonoBehaviour
+{
+    public GameObject sushi;
+    public float maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("StartSpawning", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartSpawning()
+    {
+        InvokeRepeating("SpawnSushiGroup", 1, 6f);
+    }
+
+    public void StopSpawning()
+    {
+        CancelInvoke("SpawnSushiGroup");
+        StopCoroutine("SpawnSushi");
+    }
+
+    public void SpawnSushiGroup()
+    {
+        StartCoroutine("SpawnSushi");
+    }
+
+    IEnumerator SpawnSushi()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            float Rand = Random.Range(-maxX, maxX);
+            Vector3 pos = new Vector3(Rand, transform.position.y, 0);
+            GameObject f = Instantiate(sushi, pos, Quaternion.identity) as GameObject;
+            /*add force on sushi object; so it goes up before coming down*/
+            f.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 20f), ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
